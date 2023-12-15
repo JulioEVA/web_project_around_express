4ea1150a404ac7e94ed8ad2937e0523b4f09a9e8
JulioEVA <julio-emmanuel2011@hotmail.com>
2023-12-15 16:27:37 -0700

    Add express json parser to enable route handling

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cards = require("./routes/cards");
 const users = require("./routes/users");
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.use(cards);
-app.use(users);
+mongoose.connect("mongodb://localhost:27017/aroundb");
+app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: "657bc669eb43009faff9b1ab",
+  };
+
+  next();
+});
+
+app.use("/cards", cards);
+app.use("/users", users);
 
 app.use((req, res) => {
   res.status(404).send({ message: "Requested resource not found" });
