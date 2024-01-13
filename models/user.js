b115e520ffc4dc02c56fcd5065fe058c36336bdf
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-12 20:38:56 -0700

    Refactor to use Joi validation

--- models/user.js
+++ models/user.js
@@ -1,27 +1,18 @@
 const mongoose = require("mongoose");
-const { testRegex } = require("../utils/utils");
+const { celebrate, Joi } = require("celebrate");
+const { validateURL, validateEmail } = require("../utils/utils");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+const userSchema = celebrate(
+  {
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().custom(validateURL),
+      email: Joi.string().required().custom(validateEmail),
+      password: Joi.string().required().min(6),
+    }),
   },
-  about: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  avatar: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (value) => testRegex(value),
-      message: "Invalid avatar URL",
-    },
-  },
-});
+  { abortEarly: false },
+);
 
 module.exports = mongoose.model("user", userSchema);
