7635906f4e2f52a03de63a9ca77a92656e21fd2f
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-14 20:46:01 -0700

    Add Joi validation

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
+const { validateURL } = require("../utils/utils");
+
 const {
   getCards,
   createCard,
@@ -7,10 +10,37 @@ const {
   dislikeCard,
 } = require("../controllers/cards");
 
+/**
+ * Validates the card ID.
+ */
+function validateCardId() {
+  return celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  });
+}
+
 router.get("/", getCards);
-router.post("/", createCard);
-router.delete("/:cardId", deleteCard);
-router.put("/:cardId/likes", likeCard);
-router.delete("/:cardId/likes", dislikeCard);
+router.post(
+  "/",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string()
+        .required()
+        .custom((value, helpers) => {
+          if (!validateURL(value)) {
+            return helpers.message("Invalid URL format");
+          }
+          return value;
+        }),
+    }),
+  }),
+  createCard,
+);
+router.delete("/:cardId", validateCardId, deleteCard);
+router.put("/:cardId/likes", validateCardId, likeCard);
+router.delete("/:cardId/likes", validateCardId, dislikeCard);
 
 module.exports = router;
