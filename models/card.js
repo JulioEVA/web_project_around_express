675111db99b8d7fe1c4810efb899d2d2fdbe4f90
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-14 20:42:56 -0700

    Refactor testRegex function name to match utils.js name

--- models/card.js
+++ models/card.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { testRegex } = require("../utils/utils");
+const { validateURL } = require("../utils/utils");
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -12,7 +12,7 @@ const cardSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: {
-      validator: (value) => testRegex(value),
+      validator: (value) => validateURL(value),
       message: "Invalid avatar URL",
     },
   },
