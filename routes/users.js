e7213472a9aa5a45c1f32228077d018e4bd2bb7b
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-14 20:48:29 -0700

    Add joi validation

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,51 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
+const { validateURL } = require("../utils/utils");
+
 const {
   getUser,
   getUsers,
-  createUser,
   updateUser,
   updateAvatar,
+  getMe,
 } = require("../controllers/users");
 
-router.get("/:userId", getUser);
+function validateUserId() {
+  return celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().hex().length(24),
+    }),
+  });
+}
+
+router.get("/:userId", validateUserId, getUser);
+router.get("/me", getMe);
 router.get("/", getUsers);
-router.post("/", createUser);
-router.patch("/me", updateUser);
-router.patch("/me/avatar", updateAvatar);
+router.patch(
+  "/me",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+    }),
+  }),
+  updateUser,
+);
+router.patch(
+  "/me/avatar",
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string()
+        .required()
+        .custom((value, helpers) => {
+          if (!validateURL(value)) {
+            return helpers.message("Invalid URL format");
+          }
+          return value;
+        }),
+    }),
+  }),
+  updateAvatar,
+);
 
 module.exports = router;
