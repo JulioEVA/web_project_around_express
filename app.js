09b97973a37b3e5271dc9b511297c19785d38269
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-27 19:53:43 -0700

    mend

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ const allowedCors = [
   "http://localhost:3000",
 ];
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -34,13 +41,6 @@ app.use(
     },
   }),
 );
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.options("*", cors());
 
