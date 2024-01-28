0a178c2795afb1bd38e036d0d4fbcec5cba81c8e
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-27 19:16:02 -0700

    mend

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://www.around.traveling.com.ar/*",
-      "https://www.around.traveling.com.ar/",
+      "^https://www.around.traveling.com.ar/(?:w+/?)*$",
       "http://localhost:3000",
     ],
   }),
