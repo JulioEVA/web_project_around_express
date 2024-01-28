182df686bf303b416d6168aac769b39a81df5763
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-27 19:52:19 -0700

    mend

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ app.use(
   }),
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.options("*", cors());
 
 app.use(requestLogger);
