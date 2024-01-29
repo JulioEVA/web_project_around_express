9e30103a4a56551601caec236c479447061dcb09
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-28 22:36:39 -0700

    Change to options

--- app.js
+++ app.js
@@ -16,16 +16,6 @@ app.options(
   "*",
   cors({
     // Define allowed origins, methods, and headers
-    origin:"http://localhost:3000",
-    methods: ["GET", "POST, PATCH, PUT, DELETE, OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
-    credentials: true,
-  }),
-);
-
-app.use(
-  cors({
-    // Define allowed origins, methods, and headers
     origin: "http://localhost:3000",
     methods: ["GET", "POST, PATCH, PUT, DELETE, OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
