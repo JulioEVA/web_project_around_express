d63d232dfaaa377dd414827817eca894b3eddacc
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-01-29 22:13:32 -0700

    mend

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(cors());
+app.options("*", cors());
 
 mongoose.connect("mongodb://localhost:27017/aroundb");
 app.use(express.json());
